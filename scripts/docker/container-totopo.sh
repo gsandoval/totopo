98f42ed6f97e0d20d471e167aad8d75a91184a65
Guillermo Sandoval <m@ker.gs>
2020-12-11 03:01:26 -0500

    Add cloudbuild.yaml starting script. Add container launch bash script.

---
+++ scripts/docker/container-totopo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+HTTP_PORT=${PORT:-50001}
+
+dotnet /app/Totopo.dll --http-port $HTTP_PORT --totopo-resources-path /app/resources/totopo --application-resources-path /app/resources/hackergs
