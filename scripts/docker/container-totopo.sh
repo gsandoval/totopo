9ff1684ac7221676a0de905e5161381c74e1c755
Guillermo Sandoval <m@ker.gs>
2020-12-12 18:53:43 -0500

    Change docker image from alpine based to debian.

--- scripts/docker/container-totopo.sh
+++ scripts/docker/container-totopo.sh
@@ -2,4 +2,4 @@
 
 HTTP_PORT=${PORT:-50001}
 
-strace dotnet /app/Totopo.dll --http-port $HTTP_PORT --totopo-resources-path /app/resources/totopo --application-resources-path /app/resources/hackergs
+dotnet /app/Totopo.dll --http-port $HTTP_PORT --totopo-resources-path /app/resources/totopo --application-resources-path /app/resources/hackergs
